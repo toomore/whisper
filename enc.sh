3694ccd7732201c2938a1b06c74bdb3a76d29b0b
Toomore Chiang <toomore0929@gmail.com>
2016-09-08 14:16:40 +0800

    Add whisper

--- enc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(find . -name "*.txt");do
-    gpg -vae --yes -r 422b779cd894982bc41e85ee3624cad78afc3169 $i
-    rm -rf $i
-done
---
+++ whisper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DO=${1}
+echo $DO
+
+if [ "$DO" == "new" ];
+then
+    if [ ! -r "index.txt.asc" ] && [ ! -w "index.txt" ];
+    then
+        touch "index.txt"
+    fi
+
+    if [ -r "index.txt.asc" ] && [ ! -w "index.txt" ];
+    then
+        gpg -v "index.txt.asc"
+    fi
+
+    if [ -w "index.txt" ];
+    then
+        UUID=$(uuidgen)
+        echo "$UUID $(date +"%Y%m%d%H%M%S") " >> "index.txt"
+        touch "$UUID".txt
+    fi
+elif [ "$DO" == "enc" ]; then
+    for i in $(find . -name "*.txt");do
+        gpg -vae --yes -r 422b779cd894982bc41e85ee3624cad78afc3169 $i
+        rm -rf $i
+    done
+fi
