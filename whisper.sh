c149f96accd183e3916e41d9cfabc5a769d388f8
Toomore Chiang <toomore0929@gmail.com>
2016-09-09 12:17:30 +0800

    Add color.

--- whisper.sh
+++ whisper.sh
@@ -6,25 +6,25 @@ if [ "$DO" == "new" ];
 then
     if [ ! -r "index.txt.asc" ] && [ ! -w "index.txt" ];
     then
-        echo '!: Create `index.txt`'
+        echo -e '\033[1;33m!: Create `index.txt`\033[0m'
         touch "index.txt"
     fi
 
     if [ -r "index.txt.asc" ] && [ ! -w "index.txt" ];
     then
-        echo '!: Decrypt `index.txt`'
+        echo -e '\033[1;33m!: Decrypt `index.txt`\033[0m'
         gpg -v "index.txt.asc"
     fi
 
     if [ -w "index.txt" ];
     then
         UUID=$(uuidgen)
-        echo "!: Insert record into 'index.txt' ($UUID)"
+        echo -e "\033[1;33m!: Insert record into 'index.txt' ($UUID)\033[0m"
         echo "$UUID $(date +"%FT%T%z") " >> "index.txt"
         touch "$UUID".txt
     fi
 elif [ "$DO" == "e" ]; then
-    echo '!: Encrypt all.'
+    echo -e '\033[1;33m!: Encrypt all.\033[0m'
     for i in $(find . -name "*.txt" -maxdepth 1);do
         gpg -vae --yes -r $FRP $i
         rm -rf $i
@@ -32,20 +32,24 @@ elif [ "$DO" == "e" ]; then
 
     if [ ! -d "deep" ];
     then
-        echo "!: Create 'deep'"
+        echo -e "\033[1;33m!: Create 'deep'\033[0m"
         mkdir "deep"
     fi
 
     for i in $(find . -name "*.txt.asc" -maxdepth 1);do
         if [ ! "$i" == "./index.txt.asc" ];
         then
-            echo "!: Move $i into 'deep'"
+            echo -e "\033[1;33m!: Move $i into 'deep'\033[0m"
             mv $i ./deep
         fi
     done
+elif [ "$DO" == "env" ]; then
+    gpg -k "$FRP"
 else
-    echo "whisper [cmd]"
-    echo "  cmd:"
-    echo "   - new: Create an new txt"
-    echo "   - e: encrypt all txt"
+    echo "Usage: ./whisper.sh [command]"
+    echo ""
+    echo "Commands:"
+    echo "  new  Create an new txt"
+    echo "  e    Encrypt all txt"
+    echo "  env  Show user key info"
 fi
